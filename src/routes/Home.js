37b75fc9715ab9f6ce86d0375a863eb003f50afe
dave018 <sehwanjeon0316@gmail.com>
2024-04-01 20:53:04 +0900

    [Feature] Add first Home module
    Signed-off-by: dave018 <sehwanjeon0316@gmail.com>

---
+++ src/routes/Home.js
@@ -0,0 +1,14 @@
+import minimalWelcomePage from "../img/minimal_welcome_page.webp";
+import naturalWelcomePage from "../img/natural_image.jpg";
+
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home-background">
+      <h1 className="home-title">Welcome to My Website!</h1>
+    </div>
+  );
+}
+
+export default Home;
