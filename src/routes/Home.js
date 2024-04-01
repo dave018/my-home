83645f03d1442aa7922885d34c7a297d71d64a52
dave018 <sehwanjeon0316@gmail.com>
2024-04-01 21:02:15 +0900

    [Feature] Display menu bar
    Signed-off-by: dave018 <sehwanjeon0316@gmail.com>

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,10 +2,12 @@ import minimalWelcomePage from "../img/minimal_welcome_page.webp";
 import naturalWelcomePage from "../img/natural_image.jpg";
 
 import "./Home.css";
+import Menu from "./Menu.js";
 
 function Home() {
   return (
     <div className="home-background">
+      <Menu />
       <h1 className="home-title">Welcome to My Website!</h1>
     </div>
   );
